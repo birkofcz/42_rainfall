c220e49aa311e6ba1c2e697d6c9351958d3617b3
Štěpán Beneš <104515333+birkofcz@users.noreply.github.com>
2024-08-31 16:55:09 +0200

    Update source.cpp

---
+++ level9/source.cpp
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+
+class N 
+{
+  public:
+    char argument[104];
+    int  value;
+
+  N(int value) 
+  {
+    this->value = value;
+  }
+
+  int operator+(N number) 
+  {
+    return this->value + number.value;
+  }
+
+  int operator-(N number) 
+  {
+    return this->value - number.value;
+  }
+
+  void setAnnotation(char *str) 
+  {
+    int lenght;
+
+    leght = strlen(str);
+    memcpy(this->argument, str, lenght);
+  }
+};
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    exit(1);
+  }
+
+  N *first = new N(5);
+  N *second = new N(6);
+
+  *first->setAnnotation(argv[1])
+  
+  return (*first + *second);
+}
